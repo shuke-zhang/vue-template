b01fe43e0fb0aba4d478de137b524552f35649b0
zhangenjian <shukezhang207@163.com>
2024-11-04 23:35:20 +0800

    简单测试

---
+++ src/components/eslint.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const s = ref();
+</script>
+
+<template>
+  <div class="container">sss{{}}</div>
+</template>
+
+<style scoped lang="scss"></style>
