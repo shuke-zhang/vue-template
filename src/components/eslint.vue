9bc154b338bf317f55434de535b4bd3f47b0f60a
zhangenjian <1503839962@qq.com>
2024-11-22 15:08:50 +0800

    eslint测试文件调整

--- src/components/eslint.vue
+++ src/components/eslint.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
-import type { TreeNode } from './model/test';
 import type { TestModel } from './types';
 
 import { testApi } from '@/api/test';
 
 import Comtest from './comtest.vue';
-import datas from './data';
 
 import { ref } from 'vue';
 
 const s = ref<TestModel>();
-const tree = ref<TreeNode>({});
 function handleTest() {
   return testApi();
 }
@@ -19,7 +16,6 @@ function handleTest() {
 <template>
   <div class="container">sss{{}}</div>
   <div id="ssssss" class="container" @click="handleTest">sss{{}}</div>
-  {{ datas }}
   <Comtest v-model="s" />
 </template>
 
