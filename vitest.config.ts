d762726363d5fbdcddef93964a91a8747df3fac9
zhangenjian <1503839962@qq.com>
2024-11-27 15:45:36 +0800

    优化vitest.config.ts文件

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,10 @@
+import type { ConfigEnv } from 'vitest/config';
+
 import { fileURLToPath } from 'node:url';
 
 import viteConfig from './vite.config';
 
-import {
-  mergeConfig,
-  defineConfig,
-  configDefaults,
-  ConfigEnv,
-} from 'vitest/config';
-// 假设你需要加载当前的环境变量（你可以根据需要调整）
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config';
 const mode = process.env.NODE_ENV || 'development';
 const viteConfigResult = viteConfig({ mode } as ConfigEnv);
 export default mergeConfig(
