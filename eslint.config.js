61f393b5ea9edd318e31888c258d1aad9c5baffa
shuke <zhangenjian207@gmail.com>
2025-07-28 09:37:29 +0800

    1

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import antfu from '@antfu/eslint-config';
+import antfu from '@antfu/eslint-config'
 
 export default antfu({
   ignores: ['node_modules', 'dist', 'README.md'],
@@ -19,7 +19,18 @@ export default antfu({
     'vue/custom-event-name-casing': 'off',
     'node/prefer-global/process': 'off',
     // 确保与 Prettier 的分号设置一致
-    '@stylistic/semi': ['error', 'always'],
+    '@stylistic/semi': ['warn', 'never'],
     'style/eol-last': 'off',
+    // 设置忽略规则 _xx 忽略声明未使用的警告
+    'unused-imports/no-unused-vars': [
+      'warn',
+      {
+        vars: 'all',
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
   },
-});
+})
